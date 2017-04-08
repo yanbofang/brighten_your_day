feec41b9ddfc5f962fdcc8ddf78a0a21480f5c9f
Minh Tran <thuba2011@gmail.com>
2017-04-08 19:37:21 -0400

    blog stuff

---
+++ global.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+$hostname = 'mysql13.000webhost.com';
+$dbname   = 'a9522310_admin';
+$username = 'a9522310_admin'; 
+$password = 'whatisthename';
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+
+?>
--- newPost.php
+++ newPost.php
@@ -2,18 +2,14 @@
 
 // add a new blog post
 
-if (!isset($_SESSION['userid'])) {
-	header("Location: signin.php");	
-}
-
 if (isset($_POST['submit'])) {
 	$success = true;
 	$words = trim($_POST['words']);
 
 	$date_timestamp = strtotime('today');
 	if ($success) {
-		$query = "INSERT INTO Blog (user_id, words, date_post) VALUES ";
-		if ($stmt = $conn->prepare($query." ('".$_SESSION['userid']."', ?, NOW())")) {
+		$query = "INSERT INTO Blog (words, date_post) VALUES ";
+		if ($stmt = $conn->prepare($query." (?, NOW())")) {
 			$stmt->bind_param('s', $words);
 			$stmt->execute();
 			$postid = $stmt->insert_id;
